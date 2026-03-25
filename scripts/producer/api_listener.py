ef56dddf3a5e86dea9a8441d9a5e23faec7c9458
diego huynh <hnam.d3@gmail.com>
2026-03-25 18:46:02 +0700

    Updating Docker Compose, integrating dedicated producer and consumer services. Adding requirements.txt

--- scripts/producer/api_listener.py
+++ scripts/producer/api_listener.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone, timedelta
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['kafka:9092'],
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
@@ -17,17 +17,15 @@ def on_message(ws, message):
     symbol = data['s']
     price = float(data['p'])
     quantity = float(data['q'])
-    is_buyer_maker = data['m'] # True if the order is a sell order, False if the order is a buy order
+    is_buyer_maker = data['m'] 
     event_time = datetime.fromtimestamp(data['E'] / 1000, tz=timezone(timedelta(hours=7)))
-    
+    # True if the order is a sell order, False if the order is a buy order
     trade_type = "SELL" if is_buyer_maker else "BUY"
     total_value = price * quantity
     
     if total_value > 10000:
         producer.send("crypto_trades", value=data)
-            
         print(f"| {symbol} | [{trade_type}] Price: {price:.2f} | Quantity: {quantity:.4f} | Total Value: ${total_value:,.2f}" f"| Event Time: {event_time.strftime('%Y-%m-%d %H:%M:%S UTC+7')}")
-        print(data)
 
 def on_error(ws, error):
     print(error)
